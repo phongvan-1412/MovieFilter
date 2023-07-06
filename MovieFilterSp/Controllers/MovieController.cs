5e0745b3d2ae75955d2e266167c70e9305427f6f
Pham Van <85114119+phongvan-1412@users.noreply.github.com>
2023-07-06 16:37:22 +0700

    -Client: View + Controller

---
+++ MovieFilterSp/Controllers/MovieController.cs
@@ -0,0 +1,63 @@
+﻿using MovieFilterSp.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services.Description;
+
+namespace MovieFilterSp.Controllers
+{
+    public class MovieController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewData["lstYear"] = Year();
+            //ViewData["lstGenres"] = Genres();
+            //ViewData["lstMovies"] = Movies();
+            return View();
+        }
+
+        //public ActionResult Detail(int movId)
+        //{
+        //    var service = new Service.ServiceClient();
+        //    var mov = service.GetMovieById(movId);
+        //    return View(mov);
+        //}
+
+        public List<Int64> Year()
+        {
+            List<Int64> year = new List<Int64>();
+            for (int i = 1990; i <= 2100; i++)
+            {
+                year.Add(i);
+            }
+            return year;
+        }
+
+        //public List<genre> Genres()
+        //{
+        //    var service = new Service.ServiceClient();
+        //    var arrGenres = service.GetAllGenres();
+
+        //    List<genre> lstGenres = new List<genre>();
+        //    foreach (var item in arrGenres)
+        //    {
+        //        genre genre = new genre(item);
+        //        lstGenres.Add(genre);
+        //    }
+
+        //    return lstGenres;
+        //}
+
+        //public List<movie> Movies()
+        //{
+        //    var service = new Service.ServiceClient();
+        //    var lstService = JsonConvert.DeserializeObject(service.GetAllMovies());
+        //    List<movie> lstMovies = JsonConvert.DeserializeObject<List<movie>>(lstService.ToString());
+
+        //    return lstMovies;
+        //}
+    }
+}
---
+++ MovieFilterSp/Controllers/ReviewerController.cs
@@ -0,0 +1,102 @@
+﻿using MovieFilterSp.MovieService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services.Description;
+
+namespace MovieFilterSp.Controllers
+{
+    public class ReviewerController : Controller
+    {
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //public ActionResult Register(Models.reviewer reviewer)
+        //{
+        //    var service = new ServiceClient();
+
+        //    Service.reviewer postData = new Service.reviewer
+        //    {
+        //        rev_name = reviewer.rev_name,
+        //        rev_email = reviewer.rev_email,
+        //        rev_password = EncryptPassword(reviewer.rev_password),
+        //    };
+
+        //    try
+        //    {
+        //        service.CreateReviewer(postData);
+        //        TempData["createReviewer"] = "Account registed";
+        //    }
+        //    catch (Exception)
+        //    {
+        //        TempData["createFlag"] = "Failed";
+
+        //    }
+
+        //    return RedirectToAction("Login", "Reviewer");
+        //}
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //public ActionResult Login(Models.reviewer reviewer)
+        //{
+        //    var service = new ServiceClient();
+        //    Service.reviewer accountExist = service.GetReviewerByEmail(reviewer.rev_email);
+        //    if (accountExist == null || !DecryptPassword(accountExist.rev_password).Equals(reviewer.rev_password) || !accountExist.rev_email.Equals(reviewer.rev_email))
+        //    {
+        //        TempData["accountFailed"] = "Email or password is invalid. Please try again!";
+        //    }
+        //    else
+        //    {
+        //        TempData["accountVerified"] = "Log in successfully";
+        //        Session["account"] = accountExist;
+        //    }
+
+        //    return View();
+        //}
+
+        //public ActionResult Logout()
+        //{
+        //    Session["account"] = null;
+        //    return RedirectToAction("Index", "Movie");
+        //}
+
+        public static string EncryptPassword(string password)
+        {
+            if (string.IsNullOrEmpty(password))
+            {
+                return null;
+            }
+            else
+            {
+                byte[] storePassword = ASCIIEncoding.ASCII.GetBytes(password);
+                string encryptedPassword = Convert.ToBase64String(storePassword);
+                return encryptedPassword;
+            }
+        }
+
+        public static string DecryptPassword(string password)
+        {
+            if (string.IsNullOrEmpty(password))
+            {
+                return null;
+            }
+            else
+            {
+                byte[] encryptedPassword = Convert.FromBase64String(password);
+                string decryptedPassword = ASCIIEncoding.ASCII.GetString(encryptedPassword);
+                return decryptedPassword;
+            }
+        }
+    }
+}
